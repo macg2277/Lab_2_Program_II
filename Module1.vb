4c33b63f58bc0ff46cdd0156b1ee1d3fb9fdac6e
macg2277 <cg2029012021@unab.edu.sv>
2023-10-05 19:04:09 -0600

    Initial commit

---
+++ Module1.vb
@@ -0,0 +1,48 @@
+﻿Module Module1
+
+    Sub Main()
+        producto()
+
+
+    End Sub
+    Sub producto()
+        Dim precio = 75
+        Dim cambio, pago, faltante, suma, comple As Double
+
+        Console.WriteLine("COSTO DEL PRODUCTO= 75 dolares")
+
+        Console.WriteLine("Ingrese el monto de dinero")
+        pago = Console.ReadLine()
+
+        If pago > precio Then
+            cambio = pago - precio
+            Console.WriteLine("Su cambio es " & cambio)
+            Console.WriteLine("Gracias por su compra")
+
+        ElseIf pago = precio Then
+
+            Console.WriteLine("Gracias por su compra")
+
+        ElseIf pago < precio Then
+
+            faltante = precio - pago
+
+            Console.WriteLine("Debe agregar la cantidad de " & faltante)
+            Console.WriteLine("Introduzca el pago solicitado")
+            comple = Console.ReadLine()
+
+            suma = comple + pago
+
+
+        ElseIf suma = precio Then
+            Console.WriteLine("Gracias por su compra")
+
+        Else
+            Console.WriteLine("Regrese otro dia por su producto")
+
+        End If
+
+
+    End Sub
+
+End Module
